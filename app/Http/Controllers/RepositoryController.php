bd0210ea2ac567b390b689fce633a7f1f86d34d6
JesusChamiso <jesus.chamiso@hotmail.com>
2024-11-30 16:06:49 -0400

    creacion de datos TDD

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RepositoryController extends Controller
-{
-    //
+class RepositoryController extends Controller {
+    public function index() {
+        return true;
+    }
+    public function store(Request $request) {
+        $request->user()->repositories()->create($request->all());
+        return redirect()->route('repositories.index');
+    }
 }
